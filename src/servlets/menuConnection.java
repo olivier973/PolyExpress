b1e89a4b83dd3fbd47bbba5741ab88073b9772a5
Brick44 <bnezan@free.fr>
2013-02-06 14:40:09 +0100

    j'ai mis un commentaire pour test

--- src/servlets/menuConnection.java
+++ src/servlets/menuConnection.java
@@ -12,3 +12,5 @@ public class menuConnection extends HttpServlet {
 		this.getServletContext().getRequestDispatcher( "/WEB-INF/menuConnection.jsp" ).forward( request, response );
 		}
 }
+
+//commentaire
